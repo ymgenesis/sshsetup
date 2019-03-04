caa9b92157360898731535a5e23072dcb824e025
ymgenesis <janik.matthew@gmail.com>
2019-03-03 20:38:54 -0500

    v1.2
    - Found shellcheck. Many "errors"! Fixed most of them.
    - Added while loops to option 4 and 5 so menuprompt breaks properly back to menu, instead of exiting script.

--- sshsetup.sh
+++ sshsetup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-CURRENTDIR=$(PWD)
 
 ############################
-# 	sshsetup v1.1      #
+# 	sshsetup v1.2      #
 # Written by Matthew Janik #
 ############################
 
@@ -30,7 +29,7 @@ border () {
     for (( i = 0; i < len + 4; ++i )); do
         printf '='
     done
-    printf "\n| "$C"$str"$D" |\n"
+    printf "\n| %s |\n" "$C""$str""$D"
     for (( i = 0; i < len + 4; ++i )); do
         printf '='
     done
@@ -41,13 +40,13 @@ border () {
 
 menuprompt () {
 	ANS2=n
-	while [ "$ANS2" != "y" -o "$ANS2" != "Y" ];
+	while [ "$ANS2" != "y" ] && [ "$ANS2" != "Y" ];
 	do
-		read -p "Main Menu? [y/n]: " ANS2
-		if [ "$ANS2" = "n" -o "$ANS2" = "N" ];
+		read -rp "Main Menu? [y/n]: " ANS2
+		if [ "$ANS2" = "n" ] || [ "$ANS2" = "N" ];
 		then
 			:
-		elif [ "$ANS2" = "y" -o "$ANS2" = "Y" ];
+		elif [ "$ANS2" = "y" ] || [ "$ANS2" = "Y" ];
 		then
 			echo
 			echo "Returning..."
@@ -62,13 +61,13 @@ menuprompt () {
 
 proceedprompt () {
 	ANS4=n
-	while [ "$ANS4" != "y" -o "$ANS4" != "Y" ];
+	while [ "$ANS4" != "y" ] && [ "$ANS4" != "Y" ];
 	do
-		read -p "Proceed? [y/n]: " ANS4
-		if [ "$ANS4" = "n" -o "$ANS4" = "N" ];
+		read -rp "Proceed? [y/n]: " ANS4
+		if [ "$ANS4" = "n" ] || [ "$ANS4" = "N" ];
 		then
 			:
-		elif [ "$ANS4" = "y" -o "$ANS4" = "Y" ];
+		elif [ "$ANS4" = "y" ] || [ "$ANS4" = "Y" ];
 		then
 			sleep 0.7
 			break
@@ -81,42 +80,42 @@ proceedprompt () {
 remotevarcheck () {
 		if [ -z ${PORTVAR+x} ];
 		then
-				echo ""$C"Port"$D" "$R"isn't"$D" set."
+				echo "${C}Port${D} ${R}isn't${D} set."
 				sleep 0.3
 			else
 				echo
-				echo ""$C"Port"$D" "$G"is"$D" set to "$G"$PORTVAR"$D"."
+				echo "${C}Port${D} ${G}is${D} set to ${G}${PORTVAR}${D}."
 				sleep 0.3
 		fi
 		if [ -z ${IPVAR+x} ];
 		then
-				echo ""$C"IP"$D" "$R"isn't"$D" set."
+				echo "${C}IP${D} ${R}isn't${D} set."
 				sleep 0.3
 			else
-				echo ""$C"IP"$D" "$G"is"$D" set to "$G"$IPVAR"$D"."
+				echo "${C}IP${D} ${G}is${D} set to ${G}${IPVAR}${D}."
 				sleep 0.3
 		fi
 		if [ -z ${USERVAR+x} ];
 		then
-				echo ""$C"Username"$D" "$R"isn't"$D" set."
+				echo "${C}Username${D} ${R}isn't${D} set."
 				sleep 0.3
 			else
-				echo ""$C"Username"$D" "$G"is"$D" set to "$G"$USERVAR"$D"."
+				echo "${C}Username${D} ${G}is${D} set to ${G}${USERVAR}${D}."
 				sleep 0.3
 		fi
 		if [ -z ${HOSTVAR+x} ];
 		then
-				echo ""$C"Hostname"$D" "$R"isn't"$D" set."
+				echo "${C}Hostname${D} ${R}isn't${D} set."
 				sleep 0.3
 			else
-				echo ""$C"Hostname"$D" "$G"is"$D" set to "$G"$HOSTVAR"$D"."
+				echo "${C}Hostname${D} ${G}is${D} set to ${G}${HOSTVAR}${D}."
 				sleep 0.3
 		fi
 		
 		if [ -z ${PORTVAR+x} ] && [ -z ${IPVAR+x} ] && [ -z ${USERVAR+x} ] && [ -z ${HOSTVAR+x} ];
 		then
 			echo
-			echo ""$R"All remote host variables aren't set"$D"."
+			echo "${R}All remote host variables aren't set${D}."
 			echo
 			sleep 1
 			echo "Please run option 2 first." 
@@ -139,18 +138,18 @@ do
 	clear
 	border "SSH Setup Tool"
 	echo
-	echo ""$G"1"$D" - Install/Update openssh-server (Linux apt-get)"
-	echo ""$G"2"$D" - Configure remote host variables"
-	echo ""$G"3"$D" - Setup ssh keys in ~/.ssh & transfer to remote host"
-	echo ""$G"4"$D" - Add remote host entry to ~/.ssh/config"
-	echo ""$G"5"$D" - Add remote host entry to /etc/hosts"
-	echo ""$G"6"$D" - Quit"
+	echo "${G}1${D} - Install/Update openssh-server (Linux apt-get)"
+	echo "${G}2${D} - Configure remote host variables"
+	echo "${G}3${D} - Setup ssh keys in ~/.ssh & transfer to remote host"
+	echo "${G}4${D} - Add remote host entry to ~/.ssh/config"
+	echo "${G}5${D} - Add remote host entry to /etc/hosts"
+	echo "${G}6${D} - Quit"
 	echo
-	read -p "Selection: " ANS
+	read -rp "Selection: " ANS
 
 ## 1. Install/Update openssh-server (sshd)
 	
-	if [ $ANS = 1 ];
+	if [ "$ANS" = "1" ];
 	then
 
 		clear
@@ -166,7 +165,7 @@ do
 
 ## 2. Configure remote host variables
 	
-	elif [ $ANS = 2 ];
+	elif [ "$ANS" = "2" ];
 	then
 		clear
 		border "Configure Remote Host Variables"
@@ -178,48 +177,48 @@ do
 		#Port
 		while [[ $PORTVAR = "" ]] || [[ ! "$PORTVAR" =~ ^-?[0-9]+$ ]];
 		do
-		read -p "Enter remote host's "$C"ssh port"$D": " PORTVAR
+		read -rp "Enter remote host's ${C}ssh port${D}: " PORTVAR
 			if [ "$PORTVAR" = "" ] || [[ ! "$PORTVAR" =~ ^-?[0-9]+$ ]];
 			then
-				echo ""$R"Please enter a number"$D"."
+				echo "${R}Please enter a number${D}."
 			fi
 		done
 		#IP
 		while [[ $IPVAR = "" ]] || [[ ! $IPVAR =~ ^-?[0-9.]+$ ]];
 		do
-		read -p "Enter remote host's "$C"IP address"$D": " IPVAR
+		read -rp "Enter remote host's ${C}IP address${D}: " IPVAR
 			if [ "$IPVAR" = "" ] || [[ ! "$IPVAR" =~ ^-?[0-9.]+$ ]];
 			then
-				echo ""$R"Please enter an ip (x.x.x.x)"$D"."
+				echo "${R}Please enter an ip (x.x.x.x)${D}."
 			fi
 		done
 		#Username
 		while [[ $USERVAR = "" ]] || [[ $USERVAR = *[[:space:]]* ]];
 		do
-		read -p "Enter remote host's "$C"username"$D": " USERVAR
+		read -rp "Enter remote host's ${C}username${D}: " USERVAR
 			if [ "$USERVAR" = "" ] || [[ "$USERVAR" = *[[:space:]]* ]];
 			then
-				echo ""$R"Please enter a username without spaces"$D"."
+				echo "${R}Please enter a username without spaces${D}."
 			fi
 		done
 		#Hostname
 		while [[ $HOSTVAR = "" ]] || [[ $HOSTVAR = *[[:space:]]* ]];
 		do
-		read -p "Enter remote host's "$C"hostname"$D" (used to name ssh keys and ~/.ssh/config entry): " HOSTVAR
+		read -rp "Enter remote host's ${C}hostname${D} (used to name ssh keys and ~/.ssh/config entry): " HOSTVAR
 			if [ "$HOSTVAR" = "" ] || [[ "$HOSTVAR" = *[[:space:]]* ]];
 			then
-				echo ""$R"Please enter a hostname without spaces"$D"."
+				echo "${R}Please enter a hostname without spaces${D}."
 			fi
 		done
 		echo
 		sleep 0.5
-		echo ""$G"Configured!"$D""
+		echo "${G}Configured!${D}"
 		echo
 		menuprompt
 	
 ## 3. Setup & send ssh keys to remote host
 	
-	elif [ $ANS = 3 ];
+	elif [ "$ANS" = "3" ];
 	then
 	
 		clear
@@ -227,33 +226,33 @@ do
 		echo
 		# First check to see if ~/.ssh exists. Create and continue if user agrees
 		SSHDIRVAR=y
-		while [ "$SSHDIRVAR" = "y" -o "$SSHDIRVAR" = "Y" ];
+		while [ "$SSHDIRVAR" = "y" ] || [ "$SSHDIRVAR" = "Y" ];
 		do
-			if [ ! -d $HOME/.ssh ];
+			if [ ! -d "$HOME"/.ssh ];
 			then
-			read -p ""$C"$HOME/.ssh"$D" "$R"does not exist"$D". This is where our ssh keys and config file should be stored. Create it now? [y/n]: " SSHDIRVAR
+			read -rp "${C}$HOME/.ssh${D} ${R}does not exist${D}. This is where our ssh keys and config file should be stored. Create it now? [y/n]: " SSHDIRVAR
 				
-				if [ "$SSHDIRVAR" = "y" -o "$SSHDIRVAR" = "Y" ];
+				if [ "$SSHDIRVAR" = "y" ] || [ "$SSHDIRVAR" = "Y" ];
 				then
 					echo
 					echo "Attempting to backup $HOME/.ssh as sshbak to your home folder just in case. ignore the error this will produce, as"\
 					"it means $HOME/.ssh doesn't exist, and we're clear to create it."
 					echo
 					sleep 1
-					cp -r $HOME/.ssh $HOME/sshbak
+					cp -r "$HOME"/.ssh "$HOME"/sshbak
 					echo
 					sleep 1
-					echo ""$C"Creating"$D" $HOME/.ssh..."
+					echo "${C}Creating${D} $HOME/.ssh..."
 					echo
 					sleep 1
-					mkdir $HOME/.ssh
-					echo ""$G"Created"$D"! Continuing..."
+					mkdir "$HOME"/.ssh
+					echo "${G}Created${D}! Continuing..."
 					sleep 0.5
 				
-				elif [ "$SSHDIRVAR" = "n" -o "$SSHDIRVAR" = "N" ];
+				elif [ "$SSHDIRVAR" = "n" ] || [ "$SSHDIRVAR" = "N" ];
 				then
 					echo
-					echo ""$R"Not creating"$D". This tool requires ~/.ssh/ to exist."
+					echo "${R}Not creating${D}. This tool requires ~/.ssh/ to exist."
 					echo
 					echo "Returning..."
 					echo
@@ -262,38 +261,38 @@ do
 					SSHDIRVAR=y
 
 				fi
-			elif [ -d $HOME/.ssh ];
+			elif [ -d "$HOME"/.ssh ];
 			then
 				# Continue with key creation
-				cd $HOME/.ssh
+				cd "$HOME"/.ssh
 				# Check to see if remote host variables are set
 				remotevarcheck
 				echo
 				proceedprompt
 				echo
-				echo ""$G"Creating"$D" keys "$C"$HOSTVAR"$D" & "$C"$HOSTVAR.pub"$D" at ~/.ssh/. An empty passphrase stores keys in plain text."
+				echo "${G}Creating${D} keys ${C}${HOSTVAR}${D} & ${C}${HOSTVAR}.pub${D} at ~/.ssh/. An empty passphrase stores keys in plain text."
 				echo
 				sleep 0.5
-				ssh-keygen -f $HOSTVAR;
+				ssh-keygen -f "$HOSTVAR";
 				echo
 				sleep 0.5
-				echo ""$G"Created!"$D""
+				echo "${G}Created!${D}"
 				echo
 				sleep 0.7
-				echo "Attempting to send keys to "$C"$IPVAR"$D" on ssh port "$C"$PORTVAR"$D" with username "$C"$USERVAR"$D"."
+				echo "Attempting to send keys to ${C}${IPVAR}${D} on ssh port ${C}${PORTVAR}${D} with username ${C}${USERVAR}${D}."
 				echo
-				echo ""$P"Note"$D": If this is your first time logging in to "$C"$HOSTVAR"$D", you'll need to respond 'yes' to add its"\
-				"fingerprint to your ~/.ssh/known_hosts file. You'll need to provide ssh with "$C"$USERVAR"$D"'s password on "$C"$IPVAR"$D"."
+				echo "${P}Note${D}: If this is your first time logging in to ${C}${HOSTVAR}${D}, you'll need to respond 'yes' to add its"\
+				"fingerprint to your ~/.ssh/known_hosts file. You'll need to provide ssh with ${C}${USERVAR}${D}'s password on ${C}${IPVAR}${D}."
 				echo
 				sleep 0.7
 				proceedprompt
 				echo
-				ssh-copy-id -i $HOSTVAR.pub -p $PORTVAR $USERVAR@$IPVAR
+				ssh-copy-id -i "$HOSTVAR".pub -p "$PORTVAR" "$USERVAR"@"$IPVAR"
 				sleep 0.7
-				echo ""$G"Keys transferred!"$D""
+				echo "${G}Keys transferred!${D}"
 				echo
 				sleep 0.7
-				echo ""$P"Note"$D": Passwordless login via rsa keys won't work until you run option 4 (add ~/.ssh/config entry)."
+				echo "${P}Note${D}: Passwordless login via rsa keys won't work until you run option 4 (add ~/.ssh/config entry)."
 				echo
 				sleep 0.7
 				menuprompt
@@ -303,100 +302,116 @@ do
 
 ## 4. Add remote host entry to ~/.ssh/config
 	
-	elif [ $ANS = 4 ];
+	elif [ "$ANS" = "4" ];
 	then
 	
 		clear
 		border "Add Entry to ~/.ssh/config"
-		CONFIGVAR=$HOME/.ssh/config
 		echo
-		remotevarcheck
-		echo
-		proceedprompt
-		echo
-		#Check to see if ~/.ssh/config exists, backup if it does.
-		if [ -e "$CONFIGVAR" ];
-		then
-			echo "$CONFIGVAR exists. Backing up to ~/.ssh/config.bak first, just in case."
-			cp $HOME/.ssh/config $HOME/.ssh/config.bak
-			echo
-			sleep 0.7
-			echo ""$G"Completed backup!"$D" Continuing..."
-			echo
-			sleep 0.7
-		fi
-		
-		echo "Adding the following entry to the end of "$C"$CONFIGVAR"$D":"
-		sleep 0.7
-		printf "\nHost\t$HOSTVAR\n\tHostname $IPVAR\n\tUser $USERVAR\n\tPort $PORTVAR\n\tIdentityFile $HOME/.ssh/$HOSTVAR\n"
-		echo
-		sleep 0.7
-		proceedprompt
-		echo
-		printf "\nHost\t$HOSTVAR\n\tHostname $IPVAR\n\tUser $USERVAR\n\tPort $PORTVAR\n\tIdentityFile $HOME/.ssh/$HOSTVAR\n" >> $CONFIGVAR
-		echo ""$G"Entry added!"$D" Here's what "$C"$CONFIGVAR"$D" looks like now:"
-		echo
-		sleep 0.7
-		cat $CONFIGVAR
-		echo
-		sleep 0.7
-		echo ""$R"Important"$D": You can only login to "$C"$HOSTVAR"$D" without a password by executing "$G"ssh $HOSTVAR"$D""
-		echo
-		echo "'ssh -p $PORTVAR $USERVAR@$IPVAR' will prompt you for a password"
-		echo
-		sleep 0.7
-		menuprompt
+		CONFIGVAR="$HOME"/.ssh/config
+		BACKCONFVAR=on
+		while [ "$BACKCONFVAR" = "on" ];
+		do
+			if [ -e "$CONFIGVAR" ];
+			then
+				
+				#Check for remote host variables
+				remotevarcheck
+				echo
+				proceedprompt
+				echo
+				#Backup ~/.ssh/config
+				echo "${CONFIGVAR} exists. Backing up to ~/.ssh/config.bak first, just in case."
+				cp "$HOME"/.ssh/config "$HOME"/.ssh/config.bak
+				echo
+				sleep 0.7
+				echo "${G}Completed backup!${D} Continuing..."
+				BACKCONFVAR=off
+				echo
+				sleep 0.7
+				echo "Adding the following entry to the end of ${C}${CONFIGVAR}${D}:"
+				sleep 0.7
+				printf "\nHost\t%s\n\tHostname %s\n\tUser %s\n\tPort %s\n\tIdentityFile %s/.ssh/%s\n" "$HOSTVAR" "$IPVAR" "$USERVAR" "$PORTVAR" "$HOME" "$HOSTVAR"
+				echo
+				sleep 0.7
+				proceedprompt
+				echo
+				# Print to ~/.ssh/config
+				printf "\nHost\t%s\n\tHostname %s\n\tUser %s\n\tPort %s\n\tIdentityFile %s/.ssh/%s\n" "$HOSTVAR" "$IPVAR" "$USERVAR" "$PORTVAR" "$HOME" "$HOSTVAR" >> "$CONFIGVAR"
+				echo "${G}Entry added!${D} Here's what ${C}${CONFIGVAR}${D} looks like now:"
+				echo
+				sleep 0.7
+				cat "$CONFIGVAR"
+				echo
+				sleep 0.7
+				echo "${R}Important${D}: You can only login to ${C}${HOSTVAR}${D} without a password by executing ${G}ssh ${HOSTVAR}${D}"
+				echo
+				echo "'ssh -p ${PORTVAR} ${USERVAR}@${IPVAR}' will prompt you for a password"
+				echo
+				sleep 0.7
+				menuprompt
+				break
+			fi
+		done
 
 ## 5. Add remote host entry to /etc/hosts
 	
-	elif [ $ANS = 5 ];
+	elif [ "$ANS" = "5" ];
 	then
 	
 		clear
 		border "Add Entry to /etc/hosts"
 		HOSTFILEVAR=/etc/hosts
 		echo
-		remotevarcheck
-		echo
-		proceedprompt
-		echo
-		#Check to see if /etc/hosts exists (it will exist), back it up in case.
-		if [ -e "$HOSTFILEVAR" ];
-		then
-			echo "Backing up "$C"$HOSTFILEVAR"$D" to "$C"/etc/hosts.bak"$D", just in case."
-			echo
-			echo ""$R"Important"$D": By default on most systems, "$C"$HOSTFILEVAR"$D" is owned by root. This means all regular users"\
-			"need to use "$C"sudo"$D" to manipulate the file. You may be asked for your password to access the file now."
-			sudo cp /etc/hosts /etc/hosts.bak
-			echo
-			sleep 0.7
-			echo ""$G"Completed backup!"$D" Continuing..."
-			echo
-			sleep 0.7
-		fi
-		echo "Adding the following entry to the end of "$C"$HOSTFILEVAR"$D":"
-		sleep 0.7
-		echo
-		echo "$IPVAR $HOSTVAR"
-		echo
-		sleep 0.7
-		proceedprompt
-		echo
-		echo $IPVAR $HOSTVAR | sudo tee -a /etc/hosts >/dev/null
-		echo ""$G"Entry added!"$D" Here's what "$C"$HOSTFILEVAR"$D" looks like now:"
-		echo
-		sleep 0.7
-		sudo cat $HOSTFILEVAR
-		echo
-		sleep 0.7
-		echo ""$P"Note"$D": You can now type "$C"$HOSTVAR"$D" instead of "$C"$IPVAR"$D" where necessary."
-		echo
-		sleep 0.7
-		menuprompt
+		BACKHOSTVAR=on
+		while [ "$BACKHOSTVAR" = "on" ];
+		do
+			if [ -e "$HOSTFILEVAR" ];
+			then
+			
+				#Check for remote host variables
+				remotevarcheck
+				echo
+				proceedprompt
+				echo
+				#Backup /etc/hosts
+				echo "Backing up ${C}${HOSTFILEVAR}${D} to ${C}/etc/hosts.bak${D}, just in case."
+				echo
+				echo "${R}Important${D}: By default on most systems, ${C}${HOSTFILEVAR}${D} is owned by root. This means all regular users"\
+				"need to use ${C}sudo${D} to manipulate the file. You may be asked for your password to access the file now."
+				sudo cp /etc/hosts /etc/hosts.bak
+				BACKHOSTVAR=off
+				echo
+				sleep 0.7
+				echo "${G}Completed backup${D}! Continuing..."
+				echo
+				sleep 0.7
+				echo "Adding the following entry to the end of ${C}${HOSTFILEVAR}${D}:"
+				sleep 0.7
+				echo
+				echo "${IPVAR} ${HOSTVAR}"
+				echo
+				sleep 0.7
+				proceedprompt
+				echo
+				echo "$IPVAR" "$HOSTVAR" | sudo tee -a /etc/hosts >/dev/null
+				echo "${G}Entry added${D}! Here's what ${C}${HOSTFILEVAR}${D} looks like now:"
+				echo
+				sleep 0.7
+				sudo cat "$HOSTFILEVAR"
+				echo
+				sleep 0.7
+				echo "${P}Note${D}: You can now type ${C}${HOSTVAR}${D} instead of ${C}${IPVAR}${D} where necessary."
+				echo
+				sleep 0.7
+				menuprompt
+				break
+			fi
+		done
 
 ## 6. Quitting
 	
-	elif [ $ANS = 6 ];
+	elif [ "$ANS" = "6" ];
 	then
 		echo
 		echo "Quitting..."
@@ -405,7 +420,7 @@ do
 	
 	else
 		echo
-		echo "$R"Invalid selection. Returning..."$D"
+		echo "${R}Invalid selection. Returning${D}..."
 		echo
 		sleep 0.7	
 	fi
